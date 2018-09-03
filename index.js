7668b3ba3ffe23f1d0fe780a457aa9801c3c9447
Jason To <jasto@ucdavis.edu>
2018-09-03 17:51:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   } else if (uppercase.toUpperCase(string) === lowercase){
     return "I love you, too."
-  }
+  } else if ()
+  
   
 }
